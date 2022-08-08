00963f29e895ac01efbd08df178e1046517a5a72
actuallyroy <amitkumar93525@gmail.com>
2022-08-08 09:45:18 +0530

    'make'

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var mongodb = require('mongodb');
+
+const mongoClient  = mongodb.MongoClient;
+
+
+const app = express();
+app.use(express.json())
+const dbUrl = 'mongodb+srv://actuallyroy:Pzha5qkdxQTgnyQH@cluster0.hrmerlo.mongodb.net/test'
+app.get('/', async (req, res) => {
+
+    const client = await mongoClient.connect(dbUrl);
+    
+    try {
+        let db = await client.db('testDB')
+        
+    } catch (error) {
+        
+    }
+
+})
+
+
+app.listen(8001, () => {
+    console.log("Server is running on port " + 8001)
+})
